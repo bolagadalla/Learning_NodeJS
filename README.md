2774b381dd9aa0f1a7aca44ec028a319d460bdac
Bola Gadalla <bolagadalla777@gmail.com>
2021-04-29 23:07:18 -0400

    Implemented get and post requests to get and post msgs to chat box

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Learning_NodeJS
+
+This project is meant to teach me about Node.js and some of it's popular frameworks.
+
+## What I Learned:
+    - [x] Creating a server.js file
+    - [x] Creating package.json file for dependancies
+    - [x] Install Express framework
+    - [x] Listens for requests
+    - [x] Serving a static html page
+    - [x] Implement 'get' requests
+    - [x] Implements 'post' requests
+
+## How The App Looks
+<p align="center">
+  <img src="" width="350" title="App Screenshot 1">
+</p>
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+const { response } = require('express')
 var express = require('express')
+var body = require('body-parser')
 
 // Instance of express 
 var app = express()
 
 // Serving a static html file
 app.use(express.static(__dirname))
+app.use(body.json())
+// Array of messages
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jim', message: 'Hello'},
+]
+
+// Handles 'get' requests
+app.get('/messages', (request, response) => {
+    response.send(messages)
+})
+
+// Handles 'post' requests
+app.post('/messages', (request, response) => {
+    // Adds the new message to the messages array
+    messages.push(request.body)
+    response.sendStatus(200)
+})
+
 // Listens for requests
-app.listen(3000)
+var server = app.listen(3000, () => {
+    console.log('Server is listening on port', server.address().port)
+})
